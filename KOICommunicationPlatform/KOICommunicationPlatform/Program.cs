8497c5390d8bdeea8fb1ff2cc7f037f34deb7535
Pavithra Athukorala <pavi.athukorala@gmail.com>
2024-08-08 23:53:32 +1000

    Send an email to the admin to request registration on the app during the initial running process

--- KOICommunicationPlatform/KOICommunicationPlatform/Program.cs
+++ KOICommunicationPlatform/KOICommunicationPlatform/Program.cs
@@ -7,6 +7,8 @@ using KOICommunicationPlatform.DataAccess;
 using KOICommunicationPlatform.Models;
 using KOICommunicationPlatform.DataAccess.Repository.IRepository;
 using KOICommunicationPlatform.DataAccess.Repository;
+using KOICommunicationPlatform.DataAccess.DbInitializer;
+using KOICommunicationPlatform.Utilities.EmailSender;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,11 +26,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 //    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+     .AddDefaultTokenProviders();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-//builder.Services.AddScoped<IDbInitializer, DbInitializer>();
-//builder.Services.AddSingleton<IEmailSender, EmailSender>();
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddScoped<IEmailDispatcher, EmailDispatcher>();
 
 
 builder.Services.AddRazorPages();
@@ -50,7 +53,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-//SeedDatabase();
+SeedDatabase();
 app.UseAuthentication();;
 
 app.UseAuthorization();
@@ -62,11 +65,12 @@ app.MapControllerRoute(
 // --Default code : pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-//void SeedDatabase()
-//{
-//    using (var scope = app.Services.CreateScope())
-//    {
-//        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
-//        dbInitializer.Initialize();
-//    }
-//}
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
