023ebedd667698a3c1831d19830a65300187d258
Pavithra Athukorala <pavi.athukorala@gmail.com>
2024-08-11 13:30:55 +1000

    update email message to include styles and a banner

--- KOICommunicationPlatform/KOICommunicationPlatform.DataAccess/DbInitializer/DbInitializer.cs
+++ KOICommunicationPlatform/KOICommunicationPlatform.DataAccess/DbInitializer/DbInitializer.cs
@@ -88,16 +88,20 @@ namespace KOICommunicationPlatform.DataAccess.DbInitializer
                 var subject = "Password Reset Request";
 
                 var message = $@"
-<p>Hi {user.FirstName},</p>
-
-<p>We welcome you to the KOI Communication Platform, please click the link below to reset your password:</p>
-
-<p><a href='{resetPasswordUrl}' target='_blank'>Reset Your Password</a></p>
-
-<p>If you did not request a password reset, please ignore this email. If you have any questions or need further assistance, feel free to contact our support team.</p>
-
-<p>Best regards,<br>
-The KOI Team</p>";
+<div style='font-family: Arial, sans-serif; color: #333;'>
+    <div style='background-color: #f7f7f7; padding: 20px;'>
+        <img src='https://media.licdn.com/dms/image/D563DAQGz7eF5nYkI2w/image-scale_191_1128/0/1706753270420/kings_own_institute_cover?e=2147483647&v=beta&t=VUkOybxE5Cvah5a1ghXMaFsETiQNEIP2GNFXLzkb67o' alt='KOI Banner' style='width: 100%; height: auto;'>
+    </div>
+    <div style='padding: 20px; background-color: #ffffff;'>
+        <h2 style='color: #11587d;'>Hi {user.FirstName},</h2>
+        <p>We welcome you to the KOI Communication Platform. Please click the link below to reset your password:</p>
+        <p style='text-align: left;'>
+            <a href='{resetPasswordUrl}' target='_blank' style='display: inline-block; padding: 10px 20px; background-color: #11587d; color: #ffffff; text-decoration: none; border-radius: 5px;'>Reset Your Password</a>
+        </p>
+        <p>If you did not request a password reset, please ignore this email. If you have any questions or need further assistance, feel free to contact our support team.</p>
+        <p style='color: #999;'>Best regards,<br>The KOI Team</p>
+    </div>
+</div>";
 
                 //var message = $"Please reset your password by <a href='{resetPasswordUrl}' target='_blank'>clicking here</a>.";
 
